2e822662c0ff8c81cb26c26e9b544410253550b7
Andrew (“Drew”) Wallack <c25aw2@dalton.org>
2021-12-15 21:36:03 -0500

    updated hangman

--- hangman.py
+++ hangman.py
@@ -1,22 +1,39 @@
 import random
 
+list = ["awful", "banjo", "blank", "words", "shady", "quick", "quiet", "field", "fused", "quirk", "jokes", "jerks", "pager", "froze", "quilt", "bored", "couch", "child", "doubt", "delay", "exact", "enjoy", "fraud", "forty", "grace", "guard", "house", "human", "index", "ideal", "judge", "joint", "logic", "links", "lunch", "kicks", "khaki",
+        "known", "moral", "magic", "mayor", "march", "might", "newly", "novel", "ought", "ocean", "older", "pitch", "prize", "party", "radio", "royal", "rough", "rapid", "salty", "shown", "score", "sugar", "thank", "tower", "threw", "unity", "urban", "usage", "vital", "virus", "waste", "watch", "whose", "yeild", "youth", "zoned", "zebra", "xylem", 'kimjohnun']
 
-def hangman(wordlist, wordlen, guesscount):
+
+def buildman(wordlist, wordlen, guesscount):
     hangmanguess = wordlist[random.randint(0, len(wordlist)-1)]
-    while len(hangmanguess) > wordlen:
+    print(hangmanguess)
+    while len(hangmanguess) != wordlen:
         hangmanguess = wordlist[random.randint(0, len(wordlist)-1)]
+    og = hangmanguess
+    arrhangmanguess = []
+    for letter in hangmanguess:
+        arrhangmanguess.append(letter)
+    hangmanguess = arrhangmanguess
+    print(hangmanguess)
     wrongletlist = ''
     rightletlist = []
     for x in range(wordlen):
         rightletlist.append('-')
     guesscountdis = guesscount
+    y = 1
     for x in range(guesscount):
         guess = input(f'Guess a letter. The word is {wordlen} letters long. ')
         guess = guess.lower()
         guesscountdis -= 1
-        if guess in rightletlist or guess in wrongletlist:
-            print("You can't say the same letter")
+        if len(guess) != 1:
+            print(f'Guess a letter, not {len(guess)}.')
             continue
+        if guess in rightletlist or guess in wrongletlist:
+            if guess in hangmanguess:
+                pass
+            else:
+                print("You can't say the same letter")
+                continue
         if guess not in hangmanguess:
             wrongletlist += guess
             print(
@@ -25,15 +42,22 @@ def hangman(wordlist, wordlen, guesscount):
             rightletlistcop = rightletlist.copy()
             wordlocation = hangmanguess.index(guess) + 1
             rightletlist.insert(hangmanguess.index(guess), guess)
+            hangmanguess.insert(hangmanguess.index(guess), '-')
+            hangmanguess.pop(wordlocation)
             rightletlist.pop(wordlocation)
             print(
                 f'{guess} is in the word. Right letters: {rightletlist}. {guesscountdis} guesses remaining')
-        if ''.join(rightletlist) == hangmanguess:
-            print(f'You won! The word was {hangmanguess}')
+        if ''.join(rightletlist) == og:
+            print(f'You won! The word was {og}')
             break
     else:
-        print(f'You lost! The word was {hangmanguess}')
+        print(f'You lost! The word was {og}')
 
 
-hangman(["awful", "banjo", "blank", "words", "shady", "quick", "quiet", "field", "fused", "quirk", "jokes", "jerks", "pager", "froze", "quilt", "bored", "couch", "child", "doubt", "delay", "exact", "enjoy", "fraud", "forty", "grace", "guard", "house", "human", "index", "ideal", "judge", "joint", "logic", "links", "lunch", "kicks", "khaki",
-         "known", "moral", "magic", "mayor", "march", "might", "newly", "novel", "ought", "ocean", "older", "pitch", "prize", "party", "radio", "royal", "rough", "rapid", "salty", "shown", "score", "sugar", "thank", "tower", "threw", "unity", "urban", "usage", "vital", "virus", "waste", "watch", "whose", "yeild", "youth", "zoned", "zebra", "xylem"], 5, 10)
+while True:
+    wordlength = int(
+        input('Welcome to Hangman! How long should the word be? '))
+    if wordlength == 0:
+        break
+    guesses = int(input('How many guesses would you like to have? '))
+    buildman(list, wordlength, guesses)
--- playingaround.py
+++ playingaround.py
@@ -1,3 +1,8 @@
-list = 'hey'
+def find_nth(string, substring, n):
+    if (n == 1):
+        return string.find(substring)
+    else:
+        return string.find(substring, find_nth(string, substring, n - 1) + 1)
 
-print(list.index('h'))
+
+print(find_nth('heyoheyo', 'h', 2))
