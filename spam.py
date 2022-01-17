8fd54851e3f3d246bc7bd6c32562ca873af98254
Drew Wallack <drewwallack@Drew-Senior.fios-router.home>
2022-01-17 15:35:19 -0500

    password scramble and email spam bot

--- spam.py
+++ spam.py
@@ -1,6 +1,19 @@
-import pyautogui, time
-time.sleep(10)
-stuff = 'I would love to connect with you! My email is suckmyballs@hotmail.com'
-for x in range(100):
-    pyautogui.typewrite(stuff)
+import pyautogui, time, random
+# time.sleep(3000)
+time.sleep(5)
+email_list = ['dwallack123@gmail.com', 'swallack123@gmail.com', 'kenneth_epstein@hotmail.com']
+
+for x in email_list:
+    pyautogui.keyDown('command')
+    pyautogui.keyDown('n')
+    pyautogui.keyUp('command')
+    pyautogui.keyUp('n')
+    time.sleep(1)
+    pyautogui.typewrite(x)
     pyautogui.press('enter')
+    pyautogui.press('enter')
+    pyautogui.press('enter')
+    pyautogui.typewrite(';)')
+    pyautogui.press('enter')
+    pyautogui.typewrite('balls')
+    pyautogui.hotkey('shift', 'command', 'd')
