7bc21e2644862439874ec2870b3579aae37f9f84
Andrew (“Drew”) Wallack <c25aw2@dalton.org>
2023-03-07 23:38:26 -0500

    finished snakegame

--- bot/Booking/booking.py
+++ bot/Booking/booking.py
@@ -1,12 +1,14 @@
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 class Booking(webdriver.Chrome):
-    def __init__(self, teardown, date, seats, email, password, restaurant, location, prefered_times, restaurant_code, driver_path='/Users/student/Desktop/SeleniumDrivers'):
+    def __init__(self, teardown, date, seats, email, password, restaurant, location, prefered_times, restaurant_code, reload_amount, driver_path='/Users/student/Desktop/SeleniumDrivers/chromedriver'):
         self.driver_path = driver_path
         self.teardown = teardown
         super(Booking, self).__init__()
@@ -19,25 +21,32 @@ class Booking(webdriver.Chrome):
         self.location = location
         self.prefered_times = prefered_times
         self.restaurant_code = restaurant_code
+        self.reload_amount = reload_amount
     def __exit__(self, *args):
         if self.teardown:
             return super().__exit__(*args)
             self.quit()
     def land_homepage(self, restaurant, date, seats):
-        self.get('https://www.resy.com/cities/ny/' + self.restaurant + '?date=' + self.date + '&seats=' + self.seats)
+        website = self.get('https://www.resy.com/cities/ny/' + self.restaurant + '?date=' + self.date + '&seats=' + self.seats)
     def login(self, email, password):
         log_in_button = self.find_element_by_class_name('Button--login')
         log_in_button.click()
         WebDriverWait(self, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div/div/div/div[2]/div[5]/button')))
         email_option = self.find_element_by_xpath('/html/body/div[5]/div/div/div/div[2]/div[5]/button').click()
+        WebDriverWait(self, 5).until(EC.visibility_of_element_located((By.ID, 'email')))
         email_input = self.find_element_by_id('email')
         email_input.send_keys(email)
         password_input = self.find_element_by_id('password')
         password_input.send_keys(password)
         submit = self.find_element_by_css_selector('button[type="submit"]')
         submit.click()
-    def book(self, location, prefered_times, restaurant_code, date, seats):
-        WebDriverWait(self, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'ReservationButton')))
+    def book(self, restaurant, location, prefered_times, restaurant_code, date, seats, reload_amount):
+        for i in range(8):
+            try:
+                WebDriverWait(self, 1).until(EC.element_to_be_clickable((By.CLASS_NAME, 'ReservationButton')))
+                break
+            except TimeoutException:
+                self.get('https://www.resy.com/cities/ny/' + self.restaurant + '?date=' + self.date + '&seats=' + self.seats)
         for self.time in prefered_times:
             try:
                 option = self.find_element_by_id(f'{self.restaurant_code}{self.date}/{self.date}/{self.time}/{self.seats}/{self.location}')
@@ -50,4 +59,8 @@ class Booking(webdriver.Chrome):
         WebDriverWait(self, 5).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-test-id="order_summary_page-button-book"]')))
         book_button = self.find_element_by_css_selector('button[data-test-id="order_summary_page-button-book"]')
         self.execute_script("arguments[0].click();", book_button)
+        try:
+            self.find_element_by_class_name('ConfirmationPage')
+        except NoSuchElementException:
+            self.execute_script("arguments[0].click();", book_button)
         print('booked!')
--- snakegame.py
+++ snakegame.py
@@ -3,39 +3,73 @@ import pygame
 import random
 from pygame.math import Vector2
 
+# Define colors
+BG_COLOR = (255, 243, 230)
+FRUIT_COLOR = (240, 84, 84)
+SNAKE_COLOR = (41, 39, 40)
+TEXT_COLOR = (41, 39, 40)
 
 class Main:
     def __init__(self):
         self.snake = Snake()
         self.fruit = Fruit()
+        self.score = 0
+        self.fruits_eaten = 0
 
     def update(self):
         self.snake.move_snake()
-        self.check_colision()
+        self.check_collision()
         self.check_fail()
 
     def draw_elements(self):
+        screen.fill(BG_COLOR)
         self.fruit.draw_fruit()
         self.snake.draw_snake()
+        self.draw_score()
 
-    def check_colision(self):
+    def check_collision(self):
         if self.fruit.pos == self.snake.body[0]:
             self.fruit.randomize()
             self.snake.add_block()
+            self.score += 1
+            self.fruits_eaten += 1
+            self.snake.increase_speed()
 
-    def check_fail(self ):
+    def check_fail(self):
         if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
             self.game_over()
         for block in self.snake.body[1:]:
             if block == self.snake.body[0]:
-                game_over()
+                self.game_over()
 
     def game_over(self):
-        pygame.quit()
-        sys.exit()
+        # Stop the game loop and wait for user input
+        pygame.time.set_timer(screen_update, 0)
+        game_over_text = game_font.render("You Lose", True, (255, 0, 0))
+        game_over_rect = game_over_text.get_rect(center=(cell_number*cell_size//2, cell_number*cell_size//2 - 50))
+        restart_text = game_font.render("Press R to restart", True, (0, 0, 0))
+        restart_rect = restart_text.get_rect(center=(cell_number*cell_size//2, cell_number*cell_size//2 + 50))
+        while True:
+            screen.blit(game_over_text, game_over_rect)
+            screen.blit(restart_text, restart_rect)
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                    # Restart the game
+                    main_game.__init__()
+                    pygame.time.set_timer(screen_update, 130)
+                    return
 
     def draw_score(self):
-        score_text = str(len(self.snake.body) - 3)
+        score_text = 'Score: ' + str(self.score)
+        score_surface = game_font.render(score_text, True, TEXT_COLOR)
+        score_x = int(cell_size * 1.5)
+        score_y = int(cell_size * 0.5)
+        score_rect = score_surface.get_rect(center=(score_x, score_y))
+        screen.blit(score_surface, score_rect)
 
 
 class Snake:
@@ -43,6 +77,7 @@ class Snake:
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
         self.direction = Vector2(1, 0)
         self.new_block = False
+        self.speed = 9
 
     def draw_snake(self):
         for block in self.body:
@@ -59,10 +94,13 @@ class Snake:
         body_copy = self.body[:-1]
         body_copy.insert(0, body_copy[0] + self.direction)
         self.body = body_copy[:]
+        pygame.time.delay(int(1000/self.speed))
 
     def add_block(self):
         self.new_block = True
 
+    def increase_speed(self):
+        self.speed = self.speed * 1.5
 
 class Fruit:
     def __init__(self):
@@ -78,20 +116,17 @@ class Fruit:
         self.y = random.randint(0, cell_number - 1)
         self.pos = Vector2(self.x, self.y)
 
-
-def game_over():
-    pygame.quit()
-    sys.exit()
-
-
 pygame.init()
+pygame.mixer.init()
+eat_sound = pygame.mixer.Sound('Sound_crunch.wav')
 cell_size = 40
 cell_number = 20
 screen = pygame.display.set_mode(
     (cell_number * cell_size, cell_number * cell_size))
+clock = pygame
 clock = pygame.time.Clock()
 screen_update = pygame.USEREVENT
-game_font = pygame.font.Font(None, 25)
+game_font = pygame.font.SysFont('Arial', 25)
 pygame.time.set_timer(screen_update, 130)
 main_game = Main()
 
@@ -119,4 +154,4 @@ while True:
     screen.fill(pygame.Color('white'))
     main_game.draw_elements()
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(120)
